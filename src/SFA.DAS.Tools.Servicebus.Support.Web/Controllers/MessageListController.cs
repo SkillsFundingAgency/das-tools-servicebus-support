32ebe9ec64eac3fce66262582c4eaf11a853d670
Witek <witek.poreda@digital.education.gov.uk>
2020-09-04 20:57:05 +0100

    add queue name to the list

--- src/SFA.DAS.Tools.Servicebus.Support.Web/Controllers/MessageListController.cs
+++ src/SFA.DAS.Tools.Servicebus.Support.Web/Controllers/MessageListController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Tools.Servicebus.Support.Core.Models;
 using SFA.DAS.Tools.Servicebus.Support.Infrastructure.Services;
 using SFA.DAS.Tools.Servicebus.Support.Infrastructure.Services.SvcBusService;
 using SFA.DAS.Tools.Servicebus.Support.Web.Models;
@@ -23,8 +24,25 @@ namespace SFA.DAS.Tools.Servicebus.Support.Web.Controllers
         public async Task<IActionResult> Index(string selectedQueue)
         {
             var messages = await _cosmosDbContext.GetQueueMessagesAsync(UserService.GetUserId());
+            
+            var vm = new MessageListViewModel()
+            {
+                Messages = messages,
+                QueueName = getQueueName(messages)
+            };            
 
-            return View(messages);
+            return View(vm);
+        }
+
+        private string getQueueName(IEnumerable<QueueMessage> messages)
+        {
+            var name = "";
+            if ( messages?.Count() > 0 )
+            {
+                name = messages.First().Queue;
+            }
+
+            return name;
         }
     }
 }
---
+++ src/SFA.DAS.Tools.Servicebus.Support.Web/Models/MessageListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SFA.DAS.Tools.Servicebus.Support.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Tools.Servicebus.Support.Web.Models
+{
+    public class MessageListViewModel
+    {
+        public string QueueName { get; set; }
+        public IEnumerable<QueueMessage> Messages { get; set; }
+    }
+}
