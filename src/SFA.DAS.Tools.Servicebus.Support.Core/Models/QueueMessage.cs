cb24954b4042dc716c3326a9c69922f943de53d8
dannygb <dan.beavon@education.gov.uk>
2020-09-09 11:49:22 +0100

    QUAL-581 - Adds initial layout for the message details page

--- src/SFA.DAS.Tools.Servicebus.Support.Core/Models/QueueMessage.cs
+++ src/SFA.DAS.Tools.Servicebus.Support.Core/Models/QueueMessage.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.ServiceBus;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SFA.DAS.Tools.Servicebus.Support.Core.Models
@@ -12,5 +13,10 @@ namespace SFA.DAS.Tools.Servicebus.Support.Core.Models
         public Message OriginalMessage { get; set; }
         public string Queue { get; set; }
         public bool IsReadOnly { get; set; }
+        public IEnumerable<KeyValuePair<string, object>> NServiceBusProperties => OriginalMessage.UserProperties.Where(x => x.Key.ToLower().Contains("nservicebus"));
+
+        public IEnumerable<KeyValuePair<string, object>> ServiceBusProperties => OriginalMessage.UserProperties.Where(x => !x.Key.ToLower().Contains("nservicebus"));
+
+        public string Body => Encoding.UTF8.GetString(OriginalMessage.Body);
     }
 }
