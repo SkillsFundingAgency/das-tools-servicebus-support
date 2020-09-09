9d64fc68746451c095016e21676ff98f1004623a
dannygb <dan.beavon@education.gov.uk>
2020-09-09 14:04:09 +0100

    QUAL-581 - Tweaks the layout and adds further fields

--- src/SFA.DAS.Tools.Servicebus.Support.Core/Models/QueueMessage.cs
+++ src/SFA.DAS.Tools.Servicebus.Support.Core/Models/QueueMessage.cs
@@ -13,9 +13,7 @@ namespace SFA.DAS.Tools.Servicebus.Support.Core.Models
         public Message OriginalMessage { get; set; }
         public string Queue { get; set; }
         public bool IsReadOnly { get; set; }
-        public IEnumerable<KeyValuePair<string, object>> NServiceBusProperties => OriginalMessage.UserProperties.Where(x => x.Key.ToLower().Contains("nservicebus"));
-
-        public IEnumerable<KeyValuePair<string, object>> ServiceBusProperties => OriginalMessage.UserProperties.Where(x => !x.Key.ToLower().Contains("nservicebus"));
+        public IEnumerable<KeyValuePair<string, object>> UserProperties => OriginalMessage.UserProperties.OrderBy((x => x.Key));
 
         public string Body => Encoding.UTF8.GetString(OriginalMessage.Body);
     }
