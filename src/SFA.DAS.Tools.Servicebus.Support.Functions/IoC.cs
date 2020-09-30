89090e4a3755f2d1562856acde031e531ac54241
Witek <witek.poreda@digital.education.gov.uk>
2020-09-30 11:45:46 +0100

    move IoC to separate file

---
+++ src/SFA.DAS.Tools.Servicebus.Support.Functions/IoC.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.Azure.ServiceBus.Primitives;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Tools.Servicebus.Support.Application;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.BulkCreateQueueMessages;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.DeleteQueueMessage;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.DeleteUserSession;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.SendMessages;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.GetExpiredUserSessions;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.GetMessages;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.GetQueueMessageCount;
+using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.ReceiveQueueMessages;
+using SFA.DAS.Tools.Servicebus.Support.Application.Services;
+using SFA.DAS.Tools.Servicebus.Support.Infrastructure.Services;
+using SFA.DAS.Tools.Servicebus.Support.Infrastructure.Services.SvcBusService;
+
+namespace SFA.DAS.Tools.Servicebus.Support.Functions
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddTransient<IAsbService, AsbService>(s =>
+            {
+                var serviceBusConnectionString = configuration.GetValue<string>("ServiceBusRepoSettings:ServiceBusConnectionString");
+                var connectionBuilder = new ServiceBusConnectionStringBuilder(serviceBusConnectionString);
+                var tokenProvider = TokenProvider.CreateManagedIdentityTokenProvider();
+
+                return new AsbService(s.GetService<IUserService>(),
+                    configuration,
+                    s.GetRequiredService<ILogger<AsbService>>(),
+                    tokenProvider,
+                    connectionBuilder,
+                    CreateManagementClient(connectionBuilder, tokenProvider),
+                    new BatchMessageStrategy()
+                );
+            });
+
+            services.AddTransient<ICosmosDbContext, CosmosDbContext>(s => new CosmosDbContext(s.GetRequiredService<CosmosClient>(), s.GetService<IUserService>(), configuration, s.GetRequiredService<ILogger<CosmosDbContext>>()));
+            services.AddSingleton(s =>
+            {
+                var cosmosEndpointUrl = configuration.GetValue<string>("CosmosDb:Url");
+                var cosmosAuthenticationKey = configuration.GetValue<string>("CosmosDb:AuthKey");
+
+                return new CosmosClient(cosmosEndpointUrl, cosmosAuthenticationKey, new CosmosClientOptions() { AllowBulkExecution = true });
+            });
+            services.AddTransient<IQueryHandler<GetExpiredUserSessionsQuery, GetExpiredUserSessionsQueryResponse>, GetExpiredUserSessionsQueryHandler>();
+            services.AddTransient<IQueryHandler<GetMessagesQuery, GetMessagesQueryResponse>, GetMessagesQueryHandler>();
+            services.AddTransient<ICommandHandler<DeleteUserSessionCommand, DeleteUserSessionCommandResponse>, DeleteUserSessionCommandHandler>();
+            services.AddTransient<ICommandHandler<BulkCreateQueueMessagesCommand, BulkCreateQueueMessagesCommandResponse>, BulkCreateQueueMessagesCommandHandler>();
+            services.AddTransient<IQueryHandler<ReceiveQueueMessagesQuery, ReceiveQueueMessagesQueryResponse>, ReceiveQueueMessagesQueryHandler>();
+            services.AddTransient<IQueryHandler<GetQueueMessageCountQuery, GetQueueMessageCountQueryResponse>, GetQueueMessageCountQueryHandler>();
+            services.AddTransient<ICommandHandler<SendMessagesCommand, SendMessagesCommandResponse>, SendMessagesCommandHandler>();
+            services.AddTransient<ICommandHandler<DeleteQueueMessagesCommand, DeleteQueueMessagesCommandResponse>, DeleteQueueMessagesCommandHandler>();
+            services.AddTransient<IMessageService, MessageService>(s =>
+                 new MessageService(
+                    s.GetService<ICommandHandler<BulkCreateQueueMessagesCommand, BulkCreateQueueMessagesCommandResponse>>(),
+                    s.GetService<IQueryHandler<ReceiveQueueMessagesQuery, ReceiveQueueMessagesQueryResponse>>(),
+                    s.GetService<IQueryHandler<GetQueueMessageCountQuery, GetQueueMessageCountQueryResponse>>(),
+                    s.GetService<IBatchMessageStrategy>(),
+                    s.GetRequiredService<ILogger<MessageService>>(),
+                    configuration.GetValue<int>("ServiceBusRepoSettings:PeekMessageBatchSize"),
+                    s.GetService<ICommandHandler<SendMessagesCommand, SendMessagesCommandResponse>>(),
+                    s.GetService<ICommandHandler<DeleteQueueMessagesCommand, DeleteQueueMessagesCommandResponse>>()
+                )
+            );
+
+            return services;
+        }
+
+        private static ManagementClient CreateManagementClient(ServiceBusConnectionStringBuilder connectionBuilder, TokenProvider tokenProvider)
+        {
+            if (connectionBuilder.SasKey?.Length > 0)
+            {
+                return new ManagementClient(connectionBuilder);
+            }
+            else
+            {
+                return new ManagementClient(connectionBuilder, tokenProvider);
+            }
+        }
+    }
+}
--- src/SFA.DAS.Tools.Servicebus.Support.Functions/Startup.cs
+++ src/SFA.DAS.Tools.Servicebus.Support.Functions/Startup.cs
@@ -1,27 +1,11 @@
-﻿using Microsoft.Azure.Cosmos;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Management;
-using Microsoft.Azure.ServiceBus.Primitives;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs.Host.Bindings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NLog.Extensions.Logging;
-using SFA.DAS.Tools.Servicebus.Support.Application;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.BulkCreateQueueMessages;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.DeleteQueueMessage;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.DeleteUserSession;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Commands.SendMessages;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.GetExpiredUserSessions;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.GetMessages;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.GetQueueMessageCount;
-using SFA.DAS.Tools.Servicebus.Support.Application.Queue.Queries.ReceiveQueueMessages;
-using SFA.DAS.Tools.Servicebus.Support.Application.Services;
 using SFA.DAS.Tools.Servicebus.Support.Functions;
-using SFA.DAS.Tools.Servicebus.Support.Infrastructure.Services;
-using SFA.DAS.Tools.Servicebus.Support.Infrastructure.Services.SvcBusService;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 namespace SFA.DAS.Tools.Servicebus.Support.Functions
@@ -34,57 +18,12 @@ namespace SFA.DAS.Tools.Servicebus.Support.Functions
 
             var executionContextOptions = builder.Services.BuildServiceProvider()
                 .GetService<IOptions<ExecutionContextOptions>>()
-                .Value
-            ;
+                .Value;
             var appDirectory = executionContextOptions.AppDirectory;
             var configurationService = sp.GetService<IConfiguration>();
             var configuration = LoadConfiguration(appDirectory, configurationService);
 
-
-            builder.Services.AddTransient<IAsbService, AsbService>(s =>
-            {
-                var serviceBusConnectionString = configuration.GetValue<string>("ServiceBusRepoSettings:ServiceBusConnectionString");
-                var connectionBuilder = new ServiceBusConnectionStringBuilder(serviceBusConnectionString);
-                var tokenProvider = TokenProvider.CreateManagedIdentityTokenProvider();
-
-                return new AsbService(s.GetService<IUserService>(),
-                    configuration,
-                    s.GetRequiredService<ILogger<AsbService>>(),
-                    tokenProvider,
-                    connectionBuilder,
-                    CreateManagementClient(connectionBuilder, tokenProvider),
-                    new BatchMessageStrategy()
-                );
-            });
-
-            builder.Services.AddTransient<ICosmosDbContext, CosmosDbContext>(s => new CosmosDbContext(s.GetRequiredService<CosmosClient>(), s.GetService<IUserService>(), configuration, s.GetRequiredService<ILogger<CosmosDbContext>>()));
-            builder.Services.AddSingleton(s =>
-            {
-                var cosmosEndpointUrl = configuration.GetValue<string>("CosmosDb:Url");
-                var cosmosAuthenticationKey = configuration.GetValue<string>("CosmosDb:AuthKey");
-
-                return new CosmosClient(cosmosEndpointUrl, cosmosAuthenticationKey, new CosmosClientOptions() { AllowBulkExecution = true });
-            });
-            builder.Services.AddTransient<IQueryHandler<GetExpiredUserSessionsQuery, GetExpiredUserSessionsQueryResponse>, GetExpiredUserSessionsQueryHandler>();
-            builder.Services.AddTransient<IQueryHandler<GetMessagesQuery, GetMessagesQueryResponse>, GetMessagesQueryHandler>();
-            builder.Services.AddTransient<ICommandHandler<DeleteUserSessionCommand, DeleteUserSessionCommandResponse>, DeleteUserSessionCommandHandler>();
-            builder.Services.AddTransient<ICommandHandler<BulkCreateQueueMessagesCommand, BulkCreateQueueMessagesCommandResponse>, BulkCreateQueueMessagesCommandHandler>();
-            builder.Services.AddTransient<IQueryHandler<ReceiveQueueMessagesQuery, ReceiveQueueMessagesQueryResponse>, ReceiveQueueMessagesQueryHandler>();
-            builder.Services.AddTransient<IQueryHandler<GetQueueMessageCountQuery, GetQueueMessageCountQueryResponse>, GetQueueMessageCountQueryHandler>();
-            builder.Services.AddTransient<ICommandHandler<SendMessagesCommand, SendMessagesCommandResponse>, SendMessagesCommandHandler>();
-            builder.Services.AddTransient<ICommandHandler<DeleteQueueMessagesCommand, DeleteQueueMessagesCommandResponse>, DeleteQueueMessagesCommandHandler>();
-            builder.Services.AddTransient<IMessageService, MessageService>(s =>
-                 new MessageService(
-                    s.GetService<ICommandHandler<BulkCreateQueueMessagesCommand, BulkCreateQueueMessagesCommandResponse>>(),
-                    s.GetService<IQueryHandler<ReceiveQueueMessagesQuery, ReceiveQueueMessagesQueryResponse>>(),
-                    s.GetService<IQueryHandler<GetQueueMessageCountQuery, GetQueueMessageCountQueryResponse>>(),
-                    s.GetService<IBatchMessageStrategy>(),
-                    s.GetRequiredService<ILogger<MessageService>>(),
-                    configuration.GetValue<int>("ServiceBusRepoSettings:PeekMessageBatchSize"),
-                    s.GetService<ICommandHandler<SendMessagesCommand, SendMessagesCommandResponse>>(),
-                    s.GetService<ICommandHandler<DeleteQueueMessagesCommand, DeleteQueueMessagesCommandResponse>>()                    
-                )
-            );
+            builder.Services.AddServices(configuration);
 
             var nLogConfiguration = new NLogConfiguration();
             builder.Services.AddLogging((options) =>
@@ -114,16 +53,6 @@ namespace SFA.DAS.Tools.Servicebus.Support.Functions
             return builder.Build();
         }
 
-        private ManagementClient CreateManagementClient(ServiceBusConnectionStringBuilder connectionBuilder, TokenProvider tokenProvider)
-        {
-            if (connectionBuilder.SasKey?.Length > 0)
-            {
-                return new ManagementClient(connectionBuilder);
-            }
-            else
-            {
-                return new ManagementClient(connectionBuilder, tokenProvider);
-            }
-        }
+
     }
 }
